0bfb71bb9b9c9270829e428212cde8653c5e08c5
Brittany D. Smith <bdaniphotography@gmail.com>
2018-03-01 16:00:35 -0600

    Switched cities.json with restaurants.json

--- foodie-list.js
+++ foodie-list.js
@@ -11,10 +11,10 @@ xhr.onload = function() {
         let responseObject = JSON.parse(xhr.responseText);
 // Sets up a new variable called "newContent" that will eventually hold the HTML to be filled using the "for" loop below
         var newContent = "";
-        for (let i = 0; i < responseObject.cities.length; i++) {
+        for (let i = 0; i < responseObject.restaurants.length; i++) {
             newContent += '<div class="event">';
-            newContent += '<h2>City: ' + responseObject.cities[i].city + '<h2>';
-            newContent += '<p>Trip Purpose: ' + responseObject.cities[i].trip_purpose + '</p>';
+            newContent += '<h2>Restaurant: ' + responseObject.restaurants[i].restaurant + '<h2>';
+            newContent += '<p>My Rating: ' + responseObject.restaurants[i].my_rating + '</p>';
             newContent += '</div>';
         }
 // Grabs the empty <div> from index.html with the ID of "content" and fills it with "newContent"
@@ -23,6 +23,6 @@ xhr.onload = function() {
 };
 
 // Prepares the request
-xhr.open('GET', 'cities.json', true);
+xhr.open('GET', 'restaurants.json', true);
 // Sends the request
 xhr.send(null);
