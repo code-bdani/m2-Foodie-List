a218e65ff3e7d8c7a7f77e2d38e5b1d3f7d8cfc0
Brittany D. Smith <bdaniphotography@gmail.com>
2018-03-04 15:08:54 -0600

    Tried to promisify xhr request

--- foodie-list.js
+++ foodie-list.js
@@ -1,31 +1,53 @@
 "use strict";
 
+/////////////// PROMISIFIES XHR REQUEST ///////////////
+
 // Sets up call to Firebase for data
 let firebase = "https://nss-exercises-database.firebaseio.com/foodie";
+console.log(firebase);
 
+let request = obj => {
+    return new Promise((resolve, reject) => {
 // Sets up a new XMLHttpReuest object
-var xhr = new XMLHttpRequest();
-
-// On page load,...
-xhr.onload = function() {
-    // if the server status is alright...
-    if (xhr.status === 200) {
-        // Create a new variable called "responseObject" that parses the JSON data into a JavaScript object and stores it
-        let responseObject = JSON.parse(xhr.responseText);
-// Sets up a new variable called "newContent" that will eventually hold the HTML to be filled using the "for" loop below
-        var newContent = "";
-        for (let i = 0; i < responseObject.length; i++) {
-            newContent += '<div class="col-4 border rounded text-uppercase text-light bg-dark">';
-            newContent += '<h3>' + responseObject[i].restaurant + '</h3>';
-            newContent += '<p class="lead text-info">My Rating: ' + responseObject[i].my_rating + '</p>';
-            newContent += '</div>';
-        }
-// Grabs the empty <div> from index.html with the ID of "content" and fills it with "newContent"
-        document.getElementById('content').innerHTML = newContent;
-    }
-};
-
+        let xhr = new XMLHttpRequest();
 // Prepares the request and pulls data from Firebase
-xhr.open('GET', `${firebase}/restaurants.json`, true);
+        xhr.open('GET', firebase, true);
+// When the object loads,...
+        xhr.onload = () => {
+// if the status is alright...
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(JSON.parse(xhr.responseText));
+            } else {
+                reject(xhr.statusText);
+            }
+        };
+        xhr.onerror = () => reject(xhr.statusText);
 // Sends the request
-xhr.send(null);
+        xhr.send(null);
+    });
+};
+console.log(request);
+
+let promise = request(`${firebase}/restaurants.json`);
+restaurants.then(function(restaurant){
+    console.log(restaurants);
+})
+
+
+// // Create a new variable called "responseObject" that parses the JSON data into a JavaScript object and stores it
+//         let responseObject = JSON.parse(xhr.responseText);
+// // Sets up a new variable called "newContent" that will eventually hold the HTML to be filled using the "for" loop below
+//         var newContent = "";
+//         for (let i = 0; i < responseObject.length; i++) {
+//             newContent += '<div class="col-4 border rounded text-uppercase text-light bg-dark">';
+//             newContent += '<h3>' + responseObject[i].restaurant + '</h3>';
+//             newContent += '<p class="lead text-info">My Rating: ' + responseObject[i].my_rating + '</p>';
+//             newContent += '</div>';
+//         }
+// // Grabs the empty <div> from index.html with the ID of "content" and fills it with "newContent"
+//         document.getElementById('content').innerHTML = newContent;
+//     }
+// };
+
+
+// xhr.open('GET', `${firebase}/restaurants.json`, true);
